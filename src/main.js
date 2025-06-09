0316fb31c30e2a5ea3f3f3b1114361f06801e8ee
HayMiqo <mikael.gg@mail.ru>
2025-06-09 07:36:18 +0300

    fix + adaptive

--- src/main.js
+++ src/main.js
@@ -1,1 +1,19 @@
 import './style.scss'
+
+const volumeButton = document.querySelector('.volume-btn')
+const bgVideo = document.querySelector('.bg_video')
+
+let isMuted = true
+
+volumeButton.addEventListener('click', () =>{
+    isMuted = !isMuted
+
+    if (isMuted){
+        volumeButton.src = './volume_off.svg'
+        bgVideo.muted = true
+    }
+    if (!isMuted){
+        volumeButton.src = './volume_on.svg'
+        bgVideo.muted = false
+    }
+})
